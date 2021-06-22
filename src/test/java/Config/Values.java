76ec165e0b8c60499acf0bfb001e2962e25e2c91
Naymets91 <32165137+Naymets91@users.noreply.github.com>
2021-06-22 09:18:51 +0300

    +

--- src/test/java/Config/Values.java
+++ src/test/java/Config/Values.java
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class Values {
-    String userName = "1" ;
+    String userName = "" ;
     String pass = "";
     String homePage = "";
 
