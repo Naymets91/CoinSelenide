9f530b64ecbc3c1179126d1a2f8553d59d070a5c
Naymets91 <32165137+Naymets91@users.noreply.github.com>
2021-06-22 11:59:07 +0300

    Добавил метод безопасного поиска

--- src/test/java/Pages/Page.java
+++ src/test/java/Pages/Page.java
@@ -1,8 +1,20 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+
+import static com.codeborne.selenide.Selenide.$;
 
 public class Page {
 
+    public boolean find (By locator) {
+        try {
+            $(locator);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
 
 }
