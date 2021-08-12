0566c5652ae98c0d6ed4d5480980ee8ee8dc332b
Naymets91 <exibet179@gmail.com>
2021-08-12 10:59:26 +0300

    ++

---
+++ src/test/java/Config/Values.java
@@ -0,0 +1,44 @@
+package Config;
+
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class Values {
+
+    public static  String homePage = "";
+
+    public static  String ukrnetPage = "";
+
+    public static  String ukrnet_email = "";
+    public static  String ukrnet_password = "";
+    public static  String userReset_email = "";
+
+
+    public static  String userRegDelMail ="";
+    public static  String userRegDelPassword ="";
+
+    public static  String user1_email = "";
+    public static  String user1_password = "";
+
+    public static  String user2_email = "";
+    public static  String user2_password = "";
+
+    public static  String user3_Limit_email = "";
+    public static  String user3_Limit_password = "";
+
+
+    public static  String admin_email = "";
+    public static  String admin_password = "";
+    public static  String fa2_secret_key = "";
+
+
+}
+
+
+
+
+
+
+
